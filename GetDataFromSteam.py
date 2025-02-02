883bde12daadbef8a9a0746f6fab04a7211f33de
Maxim <maximus405@gmail.com>
2025-02-02 15:32:43 +0300

    Get number off current players for game, check method

--- GetDataFromSteam.py
+++ GetDataFromSteam.py
@@ -14,7 +14,7 @@ def get_popular_games():
 
 
 popular_games = get_popular_games()
-
+top1_game_id = popular_games[1]['appid']
 
 def get_game_details(app_id):
     url = f"https://store.steampowered.com/api/appdetails?appids={app_id}"
@@ -23,5 +23,16 @@ def get_game_details(app_id):
     return data[str(app_id)]['data']
 
 
-top1_details = get_game_details(popular_games[1]['appid'])
+top1_details = get_game_details(top1_game_id)
 print(top1_details)
+
+
+def get_current_players(app_id):
+    url = f"https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid={app_id}"
+    response = requests.get(url)
+    data = response.json()
+    return data['response']['player_count']
+
+
+top1_current_players = get_current_players(top1_game_id )
+print(top1_current_players)
