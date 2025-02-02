1e530cf933818b42981679feb79d5a49d2153301
Maxim <maximus405@gmail.com>
2025-02-02 16:36:43 +0300

    Create new connection to postsql db, create new table, load data from csv to table.

--- GetPopularGames.py
+++ GetPopularGames.py
@@ -3,6 +3,9 @@ import json
 import datetime
 import os
 import pandas as pd
+from sqlalchemy import create_engine, Column, Integer
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 with open('D:/steam.txt', 'r') as file:
     steam_key = file.readline().strip()
@@ -43,3 +46,46 @@ df.to_csv(f'{filename}.csv', index=False)
 
 # Delete JSON file
 os.remove(f'{filename}.json')
+
+# DATABASE PART
+with open('D:/database.txt', 'r') as file:
+    database = file.readline().strip()
+
+engine = create_engine(f'{database}')
+
+Base = declarative_base()
+
+# Create the table in the database
+Base.metadata.create_all(engine)
+
+
+class Weather(Base):
+    __tablename__ = 'top_steam_games'
+
+    req_id = Column(Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
+    rank = Column(Integer, nullable=True)
+    appid = Column(Integer, nullable=True)
+    last_week_rank = Column(Integer, nullable=True)
+    peak_in_game = Column(Integer, nullable=True)
+
+
+# Create a session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+df = pd.read_csv(f'{filename}.csv')
+
+# Insert data into the table
+df.to_sql('top_steam_games', engine, if_exists='append', index=False)
+
+print(f"Loaded data from {filename}.csv")
+
+# Commit the session
+session.commit()
+
+# Close the session
+session.close()
+
+# Deleting csv file
+os.remove(f'{filename}.csv')
+print(f'{filename}.csv successfully deleted')
