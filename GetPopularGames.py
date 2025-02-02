e47246330848592e6c53f936a07850bc6e8a6f64
Maxim <maximus405@gmail.com>
2025-02-02 15:56:24 +0300

    Update request part, add logic with creation of json file

--- GetPopularGames.py
+++ GetPopularGames.py
@@ -1,37 +1,27 @@
 import requests
+import json
+import datetime
 
 with open('D:/steam.txt', 'r') as file:
     steam_key = file.readline().strip()
 
 API_KEY = steam_key
+URL = f"https://api.steampowered.com/ISteamChartsService/GetMostPlayedGames/v1/?key={API_KEY}"
+current_datetime = datetime.datetime.now()
+year = current_datetime.year
+month = current_datetime.month
+day = current_datetime.day
+response = requests.get(URL, timeout=30)
 
+if response.status_code == 200:
+    def get_popular_games():
+        popular_games_data = response.json()
 
-def get_popular_games():
-    url = f"https://api.steampowered.com/ISteamChartsService/GetMostPlayedGames/v1/?key={API_KEY}"
-    response = requests.get(url)
-    data = response.json()
-    return data
-    #return data['response']['ranks']
+        with open(f'popular_steam_games_{year}{month}{day}.json', 'w') as file:
+            json.dump(popular_games_data, file, indent=4)
 
-
-def get_game_details(app_id):
-    url = f"https://store.steampowered.com/api/appdetails?appids={app_id}"
-    response = requests.get(url)
-    data = response.json()
-    return data[str(app_id)]['data']
-
-
-def get_current_players(app_id):
-    url = f"https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid={app_id}"
-    response = requests.get(url)
-    data = response.json()
-    return data['response']['player_count']
+        return popular_games_data
+        #return data['response']['ranks']
 
 
 popular_games = get_popular_games()
-
-for game in popular_games[:5]:
-    app_id = game['appid']
-    details = get_game_details(app_id)
-    current_players = get_current_players(app_id)
-
