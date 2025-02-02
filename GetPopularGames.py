4ae15b8265272e580f61ada8f9c776d25012abd0
Maxim <maximus405@gmail.com>
2025-02-02 20:13:41 +0300

    Update GetPopularGames, extract all logic to main function.
    Configure Details script, to get data from api and store it into json file , ready to transform to csv file and upload to DB

--- GetPopularGames.py
+++ GetPopularGames.py
@@ -3,9 +3,10 @@ import json
 import datetime
 import os
 import pandas as pd
-from sqlalchemy import create_engine, Column, Integer
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, DateTime, create_engine
+from sqlalchemy.sql import func
 
 with open('D:/steam.txt', 'r') as file:
     steam_key = file.readline().strip()
@@ -29,63 +30,68 @@ if response.status_code == 200:
             json.dump(data, file, indent=4)
 
         #return popular_games_data
-        #return data['response']['ranks']
+        return f'{filename}.json'
 
-# Call
-get_popular_games()
+def main():
+    # Call
+    get_popular_games()
 
-# Read JSON file
-with open(f'{filename}.json', 'r') as f:
-    data = json.load(f)
+    # Read JSON file
+    with open(f'{filename}.json', 'r') as f:
+        data = json.load(f)
 
-# Convert JSON to DataFrame
-df = pd.json_normalize(data)
+    # Convert JSON to DataFrame
+    df = pd.json_normalize(data)
 
-# Create CSV file
-df.to_csv(f'{filename}.csv', index=False)
+    # Create CSV file
+    df.to_csv(f'{filename}.csv', index=False)
 
-# Delete JSON file
-os.remove(f'{filename}.json')
+    # Delete JSON file
+    #os.remove(f'{filename}.json') do not delete json file from there right now
 
-# DATABASE PART
-with open('D:/database.txt', 'r') as file:
-    database = file.readline().strip()
+    # DATABASE PART
+    with open('D:/database.txt', 'r') as file:
+        database = file.readline().strip()
 
-engine = create_engine(f'{database}')
+    engine = create_engine(f'{database}')
 
-Base = declarative_base()
+    Base = declarative_base()
 
-# Create the table in the database
-Base.metadata.create_all(engine)
-
-
-class Weather(Base):
-    __tablename__ = 'top_steam_games'
-
-    req_id = Column(Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
-    rank = Column(Integer, nullable=True)
-    appid = Column(Integer, nullable=True)
-    last_week_rank = Column(Integer, nullable=True)
-    peak_in_game = Column(Integer, nullable=True)
+    # Create the table in the database
+    Base.metadata.create_all(engine)
 
 
-# Create a session
-Session = sessionmaker(bind=engine)
-session = Session()
+    class TopSteamGame(Base):
+        __tablename__ = 'top_steam_games'
 
-df = pd.read_csv(f'{filename}.csv')
+        req_id = Column(Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
+        rank = Column(Integer, nullable=True)
+        appid = Column(Integer, nullable=True)
+        last_week_rank = Column(Integer, nullable=True)
+        peak_in_game = Column(Integer, nullable=True)
+        created_at = Column(DateTime, server_default=func.now(), nullable=False)
 
-# Insert data into the table
-df.to_sql('top_steam_games', engine, if_exists='append', index=False)
 
-print(f"Loaded data from {filename}.csv")
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-# Commit the session
-session.commit()
+    df = pd.read_csv(f'{filename}.csv')
 
-# Close the session
-session.close()
+    # Insert data into the table
+    df.to_sql('top_steam_games', engine, if_exists='append', index=False)
 
-# Deleting csv file
-os.remove(f'{filename}.csv')
-print(f'{filename}.csv successfully deleted')
+    print(f"Loaded data from {filename}.csv")
+
+    # Commit the session
+    session.commit()
+
+    # Close the session
+    session.close()
+
+    # Deleting csv file
+    os.remove(f'{filename}.csv')
+    print(f'{filename}.csv successfully deleted')
+
+if __name__ == '__main__':
+    main()
--- GetPopularGamesDetails.py
+++ GetPopularGamesDetails.py
@@ -1,13 +1,76 @@
 import requests
+import json
+import time
+import datetime
+import os
+import pandas as pd
+from sqlalchemy import create_engine, Column, Integer
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+from GetPopularGames import get_popular_games
 
 with open('D:/steam.txt', 'r') as file:
     steam_key = file.readline().strip()
 
 API_KEY = steam_key
 
+popular_games_filename = get_popular_games()
+print(popular_games_filename)
 
-def get_game_details(app_id):
+# JSON read
+with open(f'{popular_games_filename}', 'r') as file:
+    data = json.load(file)
+
+# Extract appid
+appid_list = [item['appid'] for item in data]
+games_with_details = []
+
+
+def get_current_players(app_id, max_retries=5, delay=5):
     url = f"https://store.steampowered.com/api/appdetails?appids={app_id}"
-    response = requests.get(url)
-    data = response.json()
-    return data[str(app_id)]['data']
+    retries = 0
+
+    while retries < max_retries:
+        try:
+            response = requests.get(url, timeout=10)
+            if response.status_code == 200:
+                data = response.json()
+                if data[str(app_id)]['success']:
+                    game_data = data[str(app_id)]['data']
+                    selected_data = {
+                        "type": game_data.get('type'),
+                        "name": game_data.get('name'),
+                        "steam_appid": game_data.get('steam_appid'),
+                        "required_age": game_data.get('required_age'),
+                        "is_free": game_data.get('is_free'),
+                        "developers": game_data.get('developers'),
+                        "publishers": game_data.get('publishers')
+                    }
+                    print(selected_data)
+                    return selected_data
+                else:
+                    print(f"Failed to get data for app_id {app_id}")
+                    return None
+            else:
+                print(f"Request failed with status code {response.status_code}. Retrying in {delay} seconds...")
+        except requests.RequestException as e:
+            print(f"Request error: {e}. Retrying in {delay} seconds...")
+
+        retries += 1
+        time.sleep(delay)
+
+    print(f"Failed to get data for app_id {app_id} after {max_retries} attempts")
+    return None
+
+
+games_with_details = []
+for app_id in appid_list:
+    game_data = get_current_players(app_id)
+    if game_data:
+        games_with_details.append(game_data)
+
+
+print(games_with_details)
+
+with open('steam_games_details.json', 'w', encoding='utf-8') as f:
+    json.dump(games_with_details, f, ensure_ascii=False, indent=4)
